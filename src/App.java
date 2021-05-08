80cc2792d336f4f735b86bcd4e5929855fd84c0d
Douglas-Gabriel <dou-ga@hotmail.com>
2021-05-08 06:51:47 -0300

    Procedimento jogar

--- src/App.java
+++ src/App.java
@@ -62,4 +62,52 @@ public class App {
         //Exibir o campo que cruza a linha 3 com a coluna 3
         desenha(2, 2);
     }
+
+    public static void jogar(int jogador){
+        //Inicializar contador da estrutura while
+        int i = 0;
+        //Definindo o jogador da vez
+        if(jogador == 1){
+            jog = 1;
+        } else{
+            jog = 2;
+        }
+        System.out.println("\nVez do jogador: " + jog);
+        while(i == 0){
+            linha = 0; //Inicializando valor da linha
+            coluna = 0; //Inicializando valor da coluna
+            while(linha < 1 || linha > 3){
+                System.out.print("Escolha a linha (1, 2, 3):");
+                //Ler a escolha da linha
+                linha = leitor.nextInt();
+                //Aviso de linha inválida, no caso do jogador
+                //escolher uma valor menor que 1 ou maior que 3
+                if(linha < 1 || linha > 3){
+                    System.out.print("Linha inválida! Escolha uma linha entre 1 e 3.");
+                }
+            }
+            while(coluna < 1 || coluna > 3){
+                System.out.print("Escolha uma coluna (1, 2, 3):");
+                //Ler a escolha da coluna
+                coluna = leitor.nextInt();
+                //Aviso de coluna inválida, no caso do jogador
+                //escolher uma valor menor que 1 ou maior que 3
+                if(coluna < 1 || coluna > 3){
+                    System.out.print("Coluna inválida! Escolha uma coluna entre 1 e 3.");
+                }
+            }
+            //Ajusta os índices para começar do zero
+            linha = linha - 1;
+            coluna = coluna - 1;
+            if(casa[linha][coluna] == 0){
+                //Caso não estaja marcado,
+                //marca o símbolo do jogador da vez
+                casa[linha][coluna] = jog;
+                i = 1;
+            } else{
+                //Caso o campo escolhido ja estaja marcado
+                System.out.print("Posição ocupada!");
+            }
+        }
+    }
 }
